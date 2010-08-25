5729032281c824ea6810f088178de2ac52abc00d
Lorenzo Viscanti <lorenzo@mikamai.com>
2010-08-25 14:20:57 +0200

    fcgi

--- src/music/fcgi.sh
+++ src/music/fcgi.sh
@@ -1,8 +1,8 @@
 # Replace these three settings.
-PROJDIR="/var/discotheque/src/music"
+PROJDIR="/home/ubuntu/discotheque.me/src/music"
 PIDFILE="$PROJDIR/mysite.pid"
 SOCKET="/home/ubuntu/discotheque.sock"
-export PYTHONPATH=/var/discotheque/:/var/discotheque/src/:/var/django/
+export PYTHONPATH=/var/discotheque/:/home/ubuntu/discotheque.me/src/:/var/django/
 export DJANGO_SETTINGS_MODULE=music.settings
 cd $PROJDIR
 if [ -f $PIDFILE ]; then
